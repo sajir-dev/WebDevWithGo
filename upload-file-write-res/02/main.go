cde0c087330a4499cd6f95e6e1b9f0d8f2a9f1ab
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-09-24 20:25:34 +0530

    08 creating site

--- upload-file-write-res/02/main.go
+++ upload-file-write-res/02/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -14,4 +17,39 @@ func main() {
 func foo(res http.ResponseWriter, req *http.Request) {
 	// s := ""
 	fmt.Println(req.Method)
+	if req.Method == http.MethodPost {
+		// open the file
+		f, h, err := req.FormFile("q")
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+
+		fmt.Println("\nFile: ", f, "\nHeader", h, "\nerr", err)
+
+		// read
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+
+		// store on server
+		dst, err := os.Create(filepath.Join("./user/", h.FileName))
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer dst.Close()
+
+		_, err = dst.Write(bs)
+		if err != nil {
+			http.Error(res, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	res.Header().Set("Content-type", "text.html; charset=utf-8")
+	tpl.ExecuteTemplate(res, "index.gohtml", s)
 }
