6cafd93bbc3a06c3500b0f8f43acdb54c27d477d
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-10-27 19:58:08 +0530

    310 jwt learnings

--- jwt-auth2/main.go
+++ jwt-auth2/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	jwt "github.com/appleboy/gin-jwt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -11,6 +15,76 @@ func main() {
 	http.HandleFunc("/lounge", lounge)
 
 	http.ListenAndServe(":8030", nil)
+
+	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
+		Realm:       "test zone",
+		Key:         []byte("secret key"),
+		Timeout:     time.Hour,
+		MaxRefresh:  time.Hour,
+		IdentityKey: identityKey,
+		PayloadFunc: func(data interface{}) jwt.MapClaims {
+			if v, ok := data.(*User); ok {
+				return jwt.MapClaims{
+					identityKey: v.UserName,
+				}
+			}
+			return jwt.MapClaims{}
+		},
+		IdentityHandler: func(c *gin.Context) interface{} {
+			claims := jwt.ExtractClaims(c)
+			return &User{
+				UserName: claims[identityKey].(string),
+			}
+		},
+		Authenticator: func(c *gin.Context) (interface{}, error) {
+			var loginVals login
+			if err := c.ShouldBind(&loginVals); err != nil {
+				return "", jwt.ErrMissingLoginValues
+			}
+			userID := loginVals.Username
+			password := loginVals.Password
+
+			if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
+				return &User{
+					UserName:  userID,
+					LastName:  "Bo-Yi",
+					FirstName: "Wu",
+				}, nil
+			}
+
+			return nil, jwt.ErrFailedAuthentication
+		},
+		Authorizator: func(data interface{}, c *gin.Context) bool {
+			if v, ok := data.(*User); ok && v.UserName == "admin" {
+				return true
+			}
+
+			return false
+		},
+		Unauthorized: func(c *gin.Context, code int, message string) {
+			c.JSON(code, gin.H{
+				"code":    code,
+				"message": message,
+			})
+		},
+		// TokenLookup is a string in the form of "<source>:<name>" that is used
+		// to extract token from the request.
+		// Optional. Default value "header:Authorization".
+		// Possible values:
+		// - "header:<name>"
+		// - "query:<name>"
+		// - "cookie:<name>"
+		// - "param:<name>"
+		TokenLookup: "header: Authorization, query: token, cookie: jwt",
+		// TokenLookup: "query:token",
+		// TokenLookup: "cookie:token",
+
+		// TokenHeadName is a string in the header. Default value is "Bearer"
+		TokenHeadName: "Bearer",
+
+		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
+		TimeFunc: time.Now,
+	})
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
@@ -24,73 +98,3 @@ func gettoken(w http.ResponseWriter, r *http.Request) {
 func lounge(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "lounge")
 }
-
-authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
-	Realm:       "test zone",
-	Key:         []byte("secret key"),
-	Timeout:     time.Hour,
-	MaxRefresh:  time.Hour,
-	IdentityKey: identityKey,
-	PayloadFunc: func(data interface{}) jwt.MapClaims {
-		if v, ok := data.(*User); ok {
-			return jwt.MapClaims{
-				identityKey: v.UserName,
-			}
-		}
-		return jwt.MapClaims{}
-	},
-	IdentityHandler: func(c *gin.Context) interface{} {
-		claims := jwt.ExtractClaims(c)
-		return &User{
-			UserName: claims[identityKey].(string),
-		}
-	},
-	Authenticator: func(c *gin.Context) (interface{}, error) {
-		var loginVals login
-		if err := c.ShouldBind(&loginVals); err != nil {
-			return "", jwt.ErrMissingLoginValues
-		}
-		userID := loginVals.Username
-		password := loginVals.Password
-
-		if (userID == "admin" && password == "admin") || (userID == "test" && password == "test") {
-			return &User{
-				UserName:  userID,
-				LastName:  "Bo-Yi",
-				FirstName: "Wu",
-			}, nil
-		}
-
-		return nil, jwt.ErrFailedAuthentication
-	},
-	Authorizator: func(data interface{}, c *gin.Context) bool {
-		if v, ok := data.(*User); ok && v.UserName == "admin" {
-			return true
-		}
-
-		return false
-	},
-	Unauthorized: func(c *gin.Context, code int, message string) {
-		c.JSON(code, gin.H{
-			"code":    code,
-			"message": message,
-		})
-	},
-	// TokenLookup is a string in the form of "<source>:<name>" that is used
-	// to extract token from the request.
-	// Optional. Default value "header:Authorization".
-	// Possible values:
-	// - "header:<name>"
-	// - "query:<name>"
-	// - "cookie:<name>"
-	// - "param:<name>"
-	TokenLookup: "header: Authorization, query: token, cookie: jwt",
-	// TokenLookup: "query:token",
-	// TokenLookup: "cookie:token",
-
-	// TokenHeadName is a string in the header. Default value is "Bearer"
-	TokenHeadName: "Bearer",
-
-	// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
-	TimeFunc: time.Now,
-})
