be98a6bafaa11407326e5500feb46c625d7ade60
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-11-15 08:56:57 +0530

    52 Concurrency Challenges

---
+++ 52-02-DeadlockChallenge/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Challenge #1
+	c := make(chan int)
+	// c <- 1 // Created the deadlock
+	go func() { // Solution => created a goroutine that reads from the channel
+		c <- 1
+	}()
+	fmt.Println(<-c)
+
+	// Challenge #2
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i
+		}
+		// close(c)
+	}()
+
+	for n := range c {
+		fmt.Println(n)
+	}
+
+}
