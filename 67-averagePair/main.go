e0f0fa2f344eb1a1df3601414dd9f7844503c487
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-12-31 15:32:07 +0530

    67-Avg Pair

--- 67-averagePair/main.go
+++ 67-averagePair/main.go
@@ -1,24 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := []float64{1, 2, 6, 7, 8, 10}
+	var avg float64 = 10
+	fmt.Println(avgPair(a, avg))
 }
 
 func avgPair(arr []float64, avg float64) bool {
 	// avgPair([1, 2, 6 ,7, 8, 10], 7) => true
 	// avgPair([-1, 10, 100], 12) => false
 
-	// i := 0
+	i := 0
+	j := len(arr) - 1
 
-	// for j := 1; j < len(arr); j++ {
-	// 	sum := arr[j] + arr[i]
-	// 	if sum == 2*avg {
-	// 		return true
-	// 	} else if sum < 2*avg {
-	// 		i++
-	// 	} else {
-	// 		return false
-	// 	}
-	// }
+	for i < j {
+		sum := arr[j] + arr[i]
+		if sum == 2*avg {
+			return true
+		} else if sum < 2*avg {
+			i++
+		} else {
+			j--
+		}
+	}
 	return false
 }
