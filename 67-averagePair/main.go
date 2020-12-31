e598d88a370f453bfc5321a1f732cfe5347aa403
sajir-dev <62017128+sajir-dev@users.noreply.github.com>
2020-12-31 15:07:54 +0530

    67-Avg Pair

---
+++ 67-averagePair/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+func avgPair(arr []float64, avg float64) bool {
+	// avgPair([1, 2, 6 ,7, 8, 10], 7) => true
+	// avgPair([-1, 10, 100], 12) => false
+
+	// i := 0
+
+	// for j := 1; j < len(arr); j++ {
+	// 	sum := arr[j] + arr[i]
+	// 	if sum == 2*avg {
+	// 		return true
+	// 	} else if sum < 2*avg {
+	// 		i++
+	// 	} else {
+	// 		return false
+	// 	}
+	// }
+	return false
+}
